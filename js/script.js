0626f494460576d1929e14ece4e3e1ae25de26af
Iran Filho <102687095+iranfilhof@users.noreply.github.com>
2023-01-16 08:20:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { notANumber, calculateIMC } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputHeight.oninput = () => AlertError.close()
+inputWeight.oninput = () => AlertError.close()
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+  if(weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return;
+  }
+  
+  AlertError.close()
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+
+  inputHeight.value = ""
+  inputWeight.value = ""
+}
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+
+
+
+
